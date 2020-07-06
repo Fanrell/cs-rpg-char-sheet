463238163f79ffda9748f2b5d7e16eae235fd402
Fanrell <cieslik.kamil@outlook.com>
2020-07-06 22:29:15 +0200

    D100 class base

---
+++ Char_sheet/CharacterSheet/Statistics/Statistics.cs
@@ -0,0 +1,12 @@
+﻿namespace CharactertSheet
+{
+	public interface Statistics
+	{
+		public void NewStat();
+		public bool EditCoreStat(int value);
+		public bool EditFinalStat(int value);
+		public string ShowStat();
+		public string TempStat();
+		public string EditLabel(string label);
+	}
+}
---
+++ Char_sheet/CharacterSheet/Statistics/StatisticsD100.cs
@@ -0,0 +1,46 @@
+﻿namespace CharactertSheet
+{
+	public class StatisticD100 : Statistics
+	{
+		private string label;
+		private int[] stats;
+		public string Label
+		{
+			get => label;
+			set
+			{
+				if (value.Length >= 3)
+					label = value;
+			}
+		}
+		public StatisticD100(string label, int[] stat)
+		{
+			
+		}
+
+		public void NewStat()
+		{
+			string t = "t";
+		}
+		public bool EditCoreStat(int value)
+		{
+			return true;
+		}
+		public bool EditFinalStat(int value)
+        {
+			return true;
+        }
+		public string ShowStat()
+        {
+			return "t";
+        }
+		public string TempStat()
+        {
+			return "t";
+        }
+		public string EditLabel(string label)
+        {
+			return "t";
+        }
+	}
+}
